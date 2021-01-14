ef73c33209c5fd8cad2128095bf731f8da14cc42
Malo <ndekezi37@gmail.com>
2021-01-14 15:24:36 +0100

    motie de l'exercice

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+// exo1
+let personnage = {
+    nom : 'Don',
+    prenom : 'Keeper',
+    age : 24,
+    taille : 1.75,
+}
+console.log(personnage.nom);
+// exo2
+let personnage2 = {
+    nom : 'Saint',
+    prenom : 'M',
+    age : 25,
+    taille : 1.80,
+}
+
+let personnage3 = {
+    nom : 'Don',
+    age : 25,
+    taille : 1.85,
+}
+// exo4
+let personnage4 = {
+    nom : 'Man',
+    age : 25,
+    taille : 1.85,
+    saluer(){
+        console.log(`coucou ${personnage2.nom}`);
+    }
+}
+personnage4.saluer();
+
+
+// exo5
+let personnage5 = {
+    nom : 'lil',
+    prenom : 'seb',
+    age : 
+}
+
+
